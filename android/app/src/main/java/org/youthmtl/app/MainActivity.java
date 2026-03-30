610578aa569d8876a3a599f081f22e05472a32a4
Daniel Barbosa <DanielM.B.Barbosa@hotmail.com>
2026-03-29 22:04:26 -0400

    Fix admin editor layout expansion and event form overflow

---
+++ android/app/src/main/java/org/youthmtl/app/MainActivity.java
@@ -0,0 +1,30 @@
+package org.youthmtl.app;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        WebView webView = new WebView(this);
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setDatabaseEnabled(true);
+        settings.setAllowFileAccess(true);
+        settings.setAllowContentAccess(true);
+        settings.setMixedContentMode(WebSettings.MIXED_CONTENT_COMPATIBILITY_MODE);
+
+        webView.setWebViewClient(new WebViewClient());
+        setContentView(webView);
+        webView.loadUrl("file:///android_asset/index.html");
+    }
+}
---
+++ src/config.js
@@ -0,0 +1,10 @@
+export const STORAGE_KEY = 'youth-montreal-churches';
+export const LANGUAGE_KEY = 'youth-montreal-language';
+export const DEFAULT_CENTER = [45.5017, -73.5673];
+export const DEFAULT_ZOOM = 10;
+
+// Optional: set your deployed Google Apps Script web app URL for free Sheets backend sync.
+// Example: https://script.google.com/macros/s/AKfycb.../exec
+export const SHEETS_WEB_APP_URL = '';
+
+export const ADM_PASSCODE = 'youthmtl-adm';
